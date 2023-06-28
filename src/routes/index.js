af724014db2ef36f009169646398075a2d46acc8
eld <elias.dolinsek@gmail.com>
2023-06-28 16:58:54 +0200

    Update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import NotesView from '../views/NotesView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
