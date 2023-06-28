0817638f46628a582cf3d912923d522ca4583c4c
eld <elias.dolinsek@gmail.com>
2023-06-28 14:36:51 +0200

    Add instructions

--- src/main.js
+++ src/main.js
@@ -5,5 +5,9 @@ import { createPinia } from 'pinia'
 const pinia = createPinia()
 const app = createApp(App)
 
+// TODO: 1. Installiere den Router über "npm install vue-router@4"
+// TODO: 2. Definiere eine Route für home und für notes in router/index.js
+// TODO: 3. Installiere den Router in Vue
+
 app.use(pinia)
 app.mount('#app')
